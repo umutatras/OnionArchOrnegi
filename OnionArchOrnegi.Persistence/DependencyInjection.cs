2bef9929472cedcdacb34a9d0b989ed48ac4fda0
umutatras <umutatras@gmail.com>
2025-08-16 10:10:24 +0300

    Migrations denemesi yapıldı.

--- OnionArchOrnegi.Persistence/DependencyInjection.cs
+++ OnionArchOrnegi.Persistence/DependencyInjection.cs
@@ -1,5 +1,37 @@
-﻿namespace OnionArchOrnegi.Persistence;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using OnionArchOrnegi.Domain.Identity;
+using OnionArchOrnegi.Domain.Settings;
+using OnionArchOrnegi.Persistence.Contexts;
 
-public class DependencyInjection
+namespace OnionArchOrnegi.Persistence;
+
+public static class DependencyInjection
 {
+    public static IServiceCollection AddPersistence(this IServiceCollection services, IConfiguration configuration)
+    {
+        var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+        services.AddDbContext<OnionArchOrnegiDbContext>(opt => opt.UseNpgsql(connectionString));
+        services.Configure<JwtSettings>(configuration.GetSection("JwtSettings"));
+
+        services.AddIdentity<AppUser, AppRole>(options =>
+        {
+            options.User.RequireUniqueEmail = true;
+
+            options.Password.RequireNonAlphanumeric = false;
+            options.Password.RequireUppercase = false;
+            options.Password.RequireLowercase = false;
+            options.Password.RequireDigit = false;
+            options.Password.RequiredUniqueChars = 0;
+            options.Password.RequiredLength = 6;
+        })
+        .AddEntityFrameworkStores<OnionArchOrnegiDbContext>()
+        .AddDefaultTokenProviders();
+
+
+        return services;
+    }
 }
--- OnionArchOrnegi.WebAPI/Program.cs
+++ OnionArchOrnegi.WebAPI/Program.cs
@@ -1,6 +1,8 @@
+using OnionArchOrnegi.Persistence;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddPersistence(builder.Configuration);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
