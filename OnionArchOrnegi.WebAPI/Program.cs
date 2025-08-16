7402a60225b93a7f0e4c916816dd312ee42f423b
umutatras <umutatras@gmail.com>
2025-08-16 23:13:34 +0300

    Serilog projeye eklendi.

--- OnionArchOrnegi.WebAPI/Program.cs
+++ OnionArchOrnegi.WebAPI/Program.cs
@@ -2,6 +2,7 @@ using OnionArchOrnegi.Application;
 using OnionArchOrnegi.Infrastructure;
 using OnionArchOrnegi.Persistence;
 using OnionArchOrnegi.WebAPI;
+using Serilog;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddApplicaton();
@@ -13,7 +14,15 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+// Serilog konfigürasyonu
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Debug() // Log seviyesini ayarladýk
+    .WriteTo.Console()    // Konsola logla
+    .WriteTo.File("logs/log.txt", rollingInterval: RollingInterval.Day) // Dosyaya logla
+    .CreateLogger();
 
+// Serilog'u kullanmasý için builder.Host'u ayarla
+builder.Host.UseSerilog();
 var app = builder.Build();
 app.UseCors("AllowAll");
 
